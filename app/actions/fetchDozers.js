dbcbd1753c1bbb329fc948edb5afe5b0610f2856
Noah Peden <noahpeden@gmail.com>
2023-12-18 00:55:49 -0500

    Chore: more deployment fetch debuggin

--- app/actions/fetchDozers.js
+++ app/actions/fetchDozers.js
@@ -2,8 +2,18 @@
 export default async function fetchDozers() {
   try {
     const response = await fetch(
-      'https://www.cat.com/content/catdotcom/en_US/products/new/equipment/dozers/jcr:content/root/responsivegrid/productcards.feed.json'
+      'https://www.cat.com/content/catdotcom/en_US/products/new/equipment/dozers/jcr:content/root/responsivegrid/productcards.feed.json',
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      }
     );
+    console.log(response.json());
     const jsonData = await response.json();
     return jsonData;
   } catch (error) {
