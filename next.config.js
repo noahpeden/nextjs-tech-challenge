a5ad00644e9fb2dab09bd202d49b2ef224af810b
Noah Peden <noahpeden@gmail.com>
2023-12-18 00:32:08 -0500

    Chore: next config image updates

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
   },
   images: {
     domains: ['s7d2.scene7.com'],
+    deviceSizes: [640, 768, 1024, 1280, 1600],
+    imageSizes: [16, 32, 48, 64, 96],
+    path: '/_next/image',
+    loader: 'default',
   },
 };
 
